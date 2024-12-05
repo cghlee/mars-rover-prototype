42b55a572c683387fc614d06d1c08e3327f09cf1
Christopher Lee <leecgh@gmail.com>
2024-12-05 15:02:55 +0000

    Implement Rover rotation

---
+++ MarsRover.Tests/RoverTests.cs
@@ -0,0 +1,103 @@
+﻿namespace MarsRover.Tests;
+
+public class RoverTests
+{
+    #region Rotation Tests
+    [Test]
+    public void Rotation_TurnsRoverCorrectOnTurnRight()
+    {
+        // Arrange
+        Rover testRoverNorth = new Rover("testName", new RoverPosition(0, 0, Compass.North));
+        Rover testRoverEast = new Rover("testName", new RoverPosition(0, 0, Compass.East));
+        Rover testRoverSouth = new Rover("testName", new RoverPosition(0, 0, Compass.South));
+        Rover testRoverWest = new Rover("testName", new RoverPosition(0, 0, Compass.West));
+
+        // Act
+        Compass outputNorth = testRoverNorth.Rotate(Command.TurnRight);
+        Compass outputEast = testRoverEast.Rotate(Command.TurnRight);
+        Compass outputSouth = testRoverSouth.Rotate(Command.TurnRight);
+        Compass outputWest = testRoverWest.Rotate(Command.TurnRight);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(testRoverNorth.RoverPosition.Facing, Is.EqualTo(Compass.East));
+            Assert.That(testRoverEast.RoverPosition.Facing, Is.EqualTo(Compass.South));
+            Assert.That(testRoverSouth.RoverPosition.Facing, Is.EqualTo(Compass.West));
+            Assert.That(testRoverWest.RoverPosition.Facing, Is.EqualTo(Compass.North));
+        });
+    }
+    [Test]
+    public void Rotation_ReturnsCorrectOnTurnRight()
+    {
+        // Arrange
+        Rover testRoverNorth = new Rover("testName", new RoverPosition(0, 0, Compass.North));
+        Rover testRoverEast = new Rover("testName", new RoverPosition(0, 0, Compass.East));
+        Rover testRoverSouth = new Rover("testName", new RoverPosition(0, 0, Compass.South));
+        Rover testRoverWest = new Rover("testName", new RoverPosition(0, 0, Compass.West));
+
+        // Act
+        Compass outputNorth = testRoverNorth.Rotate(Command.TurnRight);
+        Compass outputEast = testRoverEast.Rotate(Command.TurnRight);
+        Compass outputSouth = testRoverSouth.Rotate(Command.TurnRight);
+        Compass outputWest = testRoverWest.Rotate(Command.TurnRight);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(outputNorth, Is.EqualTo(Compass.East));
+            Assert.That(outputEast, Is.EqualTo(Compass.South));
+            Assert.That(outputSouth, Is.EqualTo(Compass.West));
+            Assert.That(outputWest, Is.EqualTo(Compass.North));
+        });
+    }
+    [Test]
+    public void Rotation_TurnsRoverCorrectOnTurnLeft()
+    {
+        // Arrange
+        Rover testRoverNorth = new Rover("testName", new RoverPosition(0, 0, Compass.North));
+        Rover testRoverEast = new Rover("testName", new RoverPosition(0, 0, Compass.East));
+        Rover testRoverSouth = new Rover("testName", new RoverPosition(0, 0, Compass.South));
+        Rover testRoverWest = new Rover("testName", new RoverPosition(0, 0, Compass.West));
+
+        // Act
+        Compass outputNorth = testRoverNorth.Rotate(Command.TurnLeft);
+        Compass outputEast = testRoverEast.Rotate(Command.TurnLeft);
+        Compass outputSouth = testRoverSouth.Rotate(Command.TurnLeft);
+        Compass outputWest = testRoverWest.Rotate(Command.TurnLeft);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(testRoverNorth.RoverPosition.Facing, Is.EqualTo(Compass.West));
+            Assert.That(testRoverEast.RoverPosition.Facing, Is.EqualTo(Compass.North));
+            Assert.That(testRoverSouth.RoverPosition.Facing, Is.EqualTo(Compass.East));
+            Assert.That(testRoverWest.RoverPosition.Facing, Is.EqualTo(Compass.South));
+        });
+    }
+    [Test]
+    public void Rotation_ReturnsCorrectOnTurnLeft()
+    {
+        // Arrange
+        Rover testRoverNorth = new Rover("testName", new RoverPosition(0, 0, Compass.North));
+        Rover testRoverEast = new Rover("testName", new RoverPosition(0, 0, Compass.East));
+        Rover testRoverSouth = new Rover("testName", new RoverPosition(0, 0, Compass.South));
+        Rover testRoverWest = new Rover("testName", new RoverPosition(0, 0, Compass.West));
+
+        // Act
+        Compass outputNorth = testRoverNorth.Rotate(Command.TurnLeft);
+        Compass outputEast = testRoverEast.Rotate(Command.TurnLeft);
+        Compass outputSouth = testRoverSouth.Rotate(Command.TurnLeft);
+        Compass outputWest = testRoverWest.Rotate(Command.TurnLeft);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(outputNorth, Is.EqualTo(Compass.West));
+            Assert.That(outputEast, Is.EqualTo(Compass.North));
+            Assert.That(outputSouth, Is.EqualTo(Compass.East));
+            Assert.That(outputWest, Is.EqualTo(Compass.South));
+        });
+    }
+    #endregion
+}
--- MarsRover/Rover.cs
+++ MarsRover/Rover.cs
@@ -2,8 +2,8 @@
 
 internal class Rover
 {
-    string Name { get; set; }
-    RoverPosition RoverPosition { get; set; }
+    internal string Name { get; set; }
+    internal RoverPosition RoverPosition { get; set; }
 
     internal Rover(string name, RoverPosition roverPosition)
     {
@@ -11,14 +11,33 @@ internal class Rover
         RoverPosition = roverPosition;
     }
 
-    //internal Compass Rotate(Command command)
-    //{
-    //    if (command == Command.TurnRight)
-    //    {
-    //        Compass newDirection = RoverPosition.Facing switch
-    //        {
-    //            Compass.N => 
-    //        };
-    //    }
-    //}
+    internal Compass Rotate(Command command)
+    {
+        Compass newDirection = Compass.North;
+
+        if (command == Command.TurnRight)
+        {
+            newDirection = RoverPosition.Facing switch
+            {
+                Compass.North => Compass.East,
+                Compass.East => Compass.South,
+                Compass.South => Compass.West,
+                Compass.West => Compass.North
+            };
+        }
+        else if (command == Command.TurnLeft)
+        {
+            newDirection = RoverPosition.Facing switch
+            {
+                Compass.North => Compass.West,
+                Compass.West => Compass.South,
+                Compass.South => Compass.East,
+                Compass.East => Compass.North
+            };
+        }
+
+        RoverPosition.Facing = newDirection;
+
+        return newDirection;
+    }
 }
